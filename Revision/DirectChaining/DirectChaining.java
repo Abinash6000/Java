23a6bfefad2c41d8e8ee8b6af6c1ca2697d87937
Abinash <abinashdash6000@gmail.com>
2023-01-02 22:54:03 +0530

    Update

---
+++ Revision/DirectChaining/DirectChaining.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class DirectChaining {
+    LinkedList<String>[] hashTable;
+    int maxChainSize = 5;
+
+    DirectChaining(int size) {
+        hashTable = new LinkedList[size];
+    }
+
+    public int modASCIIHashFunction(String word, int M) {
+        char ch[];
+        ch = word.toCharArray();
+        int i, sum;
+        for (sum = 0, i = 0; i < word.length(); i++) {
+            sum = sum + ch[i];
+        }
+        return sum % M;
+    }
+
+    public void insertHashTable(String word) {
+        int newIndex = modASCIIHashFunction(word, hashTable.length);
+        if (hashTable[newIndex] == null) {
+            hashTable[newIndex] = new LinkedList<String>();
+            hashTable[newIndex].add(word);
+        } else {
+            hashTable[newIndex].add(word);
+        }
+    }
+
+    public void displayHashTable() {
+        if (hashTable == null) {
+            System.out.println("\n HashTable does not exists");
+            return;
+        } else {
+            System.out.println("\n----------HashTable-----------");
+            for (int i = 0; i < hashTable.length; i++) {
+                System.out.println("Index " + i + ", key: " + hashTable[i]);
+            }
+        }
+    }
+
+    public boolean searchHashTable(String word) {
+        int newIndex = modASCIIHashFunction(word, hashTable.length);
+        if (hashTable[newIndex] != null && hashTable[newIndex].contains(word)) {
+            return true;
+        } else
+            return false;
+    }
+
+    public void deleteKeyHashTable(String word) {
+        int newIndex = modASCIIHashFunction(word, hashTable.length);
+        boolean result = searchHashTable(word);
+        if (result) {
+            hashTable[newIndex].remove(word);
+            System.out.println("Value deleted");
+        } else {
+            System.out.println("Value not found!");
+        }
+    }
+}
